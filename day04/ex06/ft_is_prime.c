2119c3d9b1014bf042826c0c7c81bf3b00262a27
Simthem <themthem58@gmail.com>
2019-04-06 02:09:51 +0200

    Update ft_is_prime.c

--- day04/ex06/ft_is_prime.c
+++ day04/ex06/ft_is_prime.c
@@ -15,10 +15,10 @@ int		ft_is_prime(int nb)
 	int temp;
 
 	temp = 7;
-	if (nb == 0 || nb == 1 || nb % 2 == 0 || nb % 3 == 0)
-		return (0);
 	if (nb == 2 || nb == 3 || nb == 5 || nb == 7)
 		return (1);
+	f (nb == 0 || nb == 1 || nb % 2 == 0 || nb % 3 == 0)
+		return (0);
 	while (temp <= nb / temp)
 	{
 		if (nb % temp == 0)
