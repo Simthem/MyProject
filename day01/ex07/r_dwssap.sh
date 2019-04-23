9c989ef6d7cb1874e896477147e37bf643eaca02
Simthem <themthem58@gmail.com>
2019-04-23 19:57:52 +0200

    Update r_dwssap.sh

--- day01/ex07/r_dwssap.sh
+++ day01/ex07/r_dwssap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cat /etc/passwd | grep -v '#' | sed '1d' | awk "NR%2==0" | cut -d ':' -f1 | head -n "$((FT_LINE2 - 1))" | tail -n "$((FT_LINE2 - $FT_LINE1 + 1))" | rev | sort -r | tr '\n' ' ' | sed 's/ /, /g' | rev | sed 's/,/./' | sed 's/ //' | rev | tr -d '\n'
+cat /etc/passwd | grep -v "^#" | awk NR%2==0 | sed 's/:.*//g' | rev | sort -r | head -n ${FT_LINE2} | tail -n $(($FT_LINE2-$FT_LINE1+1)) | tr '\n' ',' | sed 's/,/, /g' | sed '$ s/,.$/./g' | tr -d '\n'
