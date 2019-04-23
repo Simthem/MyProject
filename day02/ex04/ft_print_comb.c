04055310bcfdb11f1122fa75fd0e515192563607
SimThem <themthem58@gmail.com>
2019-04-23 20:20:06 +0200

    clean day02 - ex04

--- day02/ex04/ft_print_comb.c
+++ day02/ex04/ft_print_comb.c
@@ -10,22 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+#include <unistd.h>
 
-void	ft_print_num(int a, int b, int c)
+void	ft_putchar(char c)
 {
-	if (a == '7')
+	write(1, &c, 1);
+}
+
+void	ft_print(int a, int b, int c)
+{
+	ft_putchar(a + '0');
+	ft_putchar(b + '0');
+	ft_putchar(c + '0');
+	if (a != 7 || b != 8 || c != 9)
 	{
-		ft_putchar(a);
-		ft_putchar(b);
-		ft_putchar(c);
-		ft_putchar('\0');
-	}
-	else
-	{
-		ft_putchar(a);
-		ft_putchar(b);
-		ft_putchar(c);
 		ft_putchar(',');
 		ft_putchar(' ');
 	}
@@ -37,20 +35,26 @@ void	ft_print_comb(void)
 	int b;
 	int c;
 
-	a = '0';
-	while (a <= '7')
+	a = 0;
+	while (a <= 7)
 	{
 		b = a + 1;
-		while (b <= '8')
+		while (b <= 8)
 		{
 			c = b + 1;
-			while (c <= '9')
+			while (c <= 9)
 			{
-				ft_print_num(a, b, c);
+				ft_print(a, b, c);
 				c++;
 			}
 			b++;
 		}
 		a++;
 	}
+}
+
+int	main(void)
+{
+	ft_print_comb();
+	return (0);
 }
