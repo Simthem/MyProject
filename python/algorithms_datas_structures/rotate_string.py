69601b7e015e333bde48b5ed762c98d01c41635f
SimThem <themthem58@gmail.com>
2019-05-31 02:16:56 +0200

    string manipulation - slide tools - python - leetcode

--- python/algorithms_datas_structures/rotate_string.py
+++ python/algorithms_datas_structures/rotate_string.py
@@ -1,29 +1,13 @@
-###### Does not work for "abcde" "abced", can resolve with flag but if i do ######
-###### that i will not work for "[empty]""[empty]" -- i see later ######
-
-
 def rotate_string(str1, str2):
-    i = 0;
-    j = 0;
 
-    if (str1 and not str2) or (not str1 and str2):
+    if str1 == "" and str2 == "":
+        return True;
+    if len(str1) != len(str2):
         return False;
-    if str1 and str2:
-        while str1[i] != str2[j]:
-            i += 1;
-            if i > len(str1) - 1:
-                return False;
-        count = i;
-        while str1[i] != str1[count] and str1[i] == str2[j] and str1[i]:
-            if i <= len(str1) - 1:
-                i += 1;
-            if i != count and not str2[j]:
-                return False;
-            j += 1;
-            if i > len(str1) - 1:
-                i = 0;
-    return True;
-
-str1 = "abcde";
-str2 = "cdeab";
-print(rotate_string(str1, str2));
+    for i in range(len(str2)):
+        if str1[0] == str2[i]:
+            temp = str2[i:len(str2)] + str2[0:i];
+            if str1 == temp:
+                return True;
+        
+    return False;
